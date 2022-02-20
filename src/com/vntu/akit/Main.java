14dcc632dab3146cda071ad85df7460a917fc14e
romanbffff <rsirachenko@gmail.com>
2022-02-20 19:43:28 +0200

    moved class Traine in new independent of Main

--- src/com/vntu/akit/Main.java
+++ src/com/vntu/akit/Main.java
@@ -1,29 +1,6 @@
 package com.vntu.akit;
 import java.util.Scanner;
 
-class Traine {
-    private int NumberOfTraine;
-    private String start;
-    private String finish;
-    //гетери
-    public int getNumber(){
-        return this.NumberOfTraine;
-    }
-    public String getStart(){return this.start;}
-    public String getFinish(){return this.finish;}
-
-    //сетери
-    public void setNumber(int NumberOfTraine){
-        this.NumberOfTraine = NumberOfTraine;
-    }
-    public void setStart(String start){
-        this.start = start;
-    }
-    public void setFinish(String finish) {
-        this.finish = finish;
-    }
-}
-
 public class Main {
 
     public static void main(String[] args) {
